e2254c3da4157e55df8171dd95033822c0521c80
DJStefan <stefan.kussmaul@gmail.com>
2022-11-08 21:19:34 +0100

    Update README, comment parser.py

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # google-location-history-analysis
 
-Python scripts to analyze and visualize location history from Google Takeout. Uses Pandas, Matplotlib, and Jupyter Notebook. Python 3.9
+Python scripts and notebooks to analyze and visualize location history data from Google Takeout. Uses Pandas, Matplotlib, and Jupyter Notebook. Tested with Python 3.9 on a Windows system. 
+- `history_parser` contains code for parsing Google Takeout data
+- `analyze_activities.ipynb` is a notebook for analyzing ActivitySegments
+- `analyze_places.ipynb` is a notebook for analyzing PlaceVisits
+- `prepare_data.py` is a script that parses Google Takeout data and generates one CSV file containing ActivitySegment data and one containing PlaceVisit data
 
 # Set up
 
@@ -9,9 +13,20 @@ Install the requirements:
 python -m pip install -r requirements.txt
 ```
 
+Prepare your Takeout data:
+```shell
+python prepare_data.py <INSERT_PATH_TO_TAKEOUT_ZIP>
+```
+
 Start Jupyter Notebook:
 ```shell
 python -m jupyter notebook
 ```
 
-In your browser, go to the link shown in the command line, e.g. `http://localhost:8888/?token=...`
+In your browser, go to the link shown in the command line, e.g. `http://localhost:8888/?token=...`. The notebooks are `analyze_activities.ipynb` and `analyze_places.ipynb`.
+
+# Ideas for Extension
+
+- Join location data with the [Google Maps API](https://developers.google.com/maps/documentation/places/web-service/place-id) (via Place IDs) or use the lat/lon coordinates with an API such as [PositionStack](https://positionstack.com/documentation). However, I'm not sure whether that would bring any interesting insights
+- Create a map visualization (e.g. using [Geopandas](https://geopandas.org/) with a heatmap of countries visited 
+- Create a map visualization and draw the actual ActivitySegments on it using their lat/lon coordinates
--- history_parser/parser.py
+++ history_parser/parser.py
@@ -1,5 +1,5 @@
+"""Utility methods for parsing Google Takeout data."""
 import json
-import logging
 import dataclasses as dc
 from pathlib import Path
 from typing import List, Dict
@@ -7,6 +7,7 @@ from typing import List, Dict
 
 @dc.dataclass
 class ActivitySegment:
+    """Represent an ActivitySegment record. Does not represent all fields."""
     start_lat_e7: int
     start_lon_e7: int
     end_lat_e7: int
@@ -16,15 +17,16 @@ class ActivitySegment:
     distance: int
     activity_type: str
     confidence: str
-    # TODO: waypoint data vs activity data?
-    travel_distance_meters: float
+    # Travel distance (meters) derived from waypoints
+    travel_distance: float
 
 
 @dc.dataclass
 class PlaceVisit:
+    """Represent a PlaceVisit record. Does not represent all fields."""
     lat_e7: int
     lon_e7: int
-    # TODO: this may cause problems in CSV format, because addresses have commas
+    # Note: address will usually contain commas. Keep in mind when writing to CSV.
     address: str
     name: str
     place_id: str
@@ -34,6 +36,9 @@ class PlaceVisit:
 
 
 def parse_activity_segment(data: Dict) -> ActivitySegment:
+    """Parse an 'activitySegment' JSON record."""
+    assert 'activitySegment' in data
+    data = data['activitySegment']
     # Note: I use .get() here because some values may be missing,
     # and we just store them as None
     if 'waypointPath' in data:
@@ -60,6 +65,9 @@ def parse_activity_segment(data: Dict) -> ActivitySegment:
 
 
 def parse_place_visit(data: Dict) -> PlaceVisit:
+    """Parse a 'placeVisit' JSON record."""
+    assert 'placeVisit' in data
+    data = data['placeVisit']
     return PlaceVisit(
         data.get('location', {}).get('latitudeE7'),
         data.get('location', {}).get('longitudeE7'),
@@ -77,24 +85,16 @@ def parse_history(
         activity_segments: List[ActivitySegment],
         place_visits: List[PlaceVisit],
 ):
+    """
+    Parse an individual Semantic Location History JSON file and
+    write the parsed objects to the provided lists in-place.
+    """
     with open(filepath, encoding='utf-8') as f:
         data = json.load(f)
     for record in data['timelineObjects']:
         if 'activitySegment' in record:
-            activity_segments.append(parse_activity_segment(record['activitySegment']))
+            activity_segments.append(parse_activity_segment(record))
         elif 'placeVisit' in record:
-            place_visits.append(parse_place_visit(record['placeVisit']))
+            place_visits.append(parse_place_visit(record))
         else:
             raise NotImplementedError()
-
-
-@dc.dataclass
-class LocationHistory:
-    activity_segments: List[ActivitySegment] = dc.field(default_factory=list)
-    place_visits: List[PlaceVisit] = dc.field(default_factory=list)
-
-
-def parse_history_util(filepath: Path) -> LocationHistory:
-    history = LocationHistory()
-    parse_history(filepath, history.activity_segments, history.place_visits)
-    return history
